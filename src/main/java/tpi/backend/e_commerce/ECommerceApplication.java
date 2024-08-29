6861e6d377853e90e44e76600a62f211b71580dc
Felipe Del Zoppo <felipedelzoppo@gmail.com>
2024-08-29 19:48:05 -0300

    prueba de push

--- src/main/java/tpi/backend/e_commerce/ECommerceApplication.java
+++ src/main/java/tpi/backend/e_commerce/ECommerceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ECommerceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //prueba
 		SpringApplication.run(ECommerceApplication.class, args);
 	}
 
