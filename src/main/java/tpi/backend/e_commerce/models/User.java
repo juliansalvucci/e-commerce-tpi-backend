3e561f009e2a2cf8a95a83a007742d7953168652
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2024-09-07 19:27:29 -0300

    clase usuario

---
+++ src/main/java/tpi/backend/e_commerce/models/User.java
@@ -0,0 +1,20 @@
+package tpi.backend.e_commerce.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+
+    private String password;
+}
