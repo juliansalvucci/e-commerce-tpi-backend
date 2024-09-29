bbee3e8cbb70d48ede1b5278f1378215aaf3100a
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2024-09-29 17:43:09 -0300

    test endpoint crear producto V1

--- src/test/java/tpi/backend/e_commerce/ProductTest.java
+++ src/test/java/tpi/backend/e_commerce/ProductTest.java
@@ -12,6 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import jakarta.transaction.Transactional;
 import tpi.backend.e_commerce.dto.ProductDTO.CreateProductDTO;
 
 @SpringBootTest
@@ -25,11 +26,12 @@ public class ProductTest {
         private ObjectMapper objectMapper;
 
         @Test
+        @Transactional
         void testCreateProductWithNonExistentSubCategoryId() throws Exception {
                 // Crear un objeto CreateProductDTO con un subCategoryId que no existe
                 CreateProductDTO productDto = new CreateProductDTO(
-                                "Producto de prueba",
-                                "descripción",
+                                "Producto de prueba de categoría",
+                                "subcategoría inexistente",
                                 100.0,
                                 50L,
                                 10L,
@@ -44,55 +46,52 @@ public class ProductTest {
                                 .andExpect(status().isNotFound()) // Asegura que el estatus sea 404 Not Found
                                 .andExpect(jsonPath("$.subCategoryId").value("La sub categoria ingresada no existe"));
         }
-        /*
-         * @Test
-         * void testCreateProductWithValidData() throws Exception {
-         * CreateProductDTO productDto = new CreateProductDTO(
-         * "Producto de prueba",
-         * "descripción",
-         * 100.0,
-         * 50L,
-         * 10L,
-         * "http://example.com/image.jpg",
-         * 1L,
-         * 2L);
-         * 
-         * // Realizar la solicitud POST simulada
-         * mockMvc.perform(post("/product")
-         * .contentType(MediaType.APPLICATION_JSON)
-         * .content(objectMapper.writeValueAsString(productDto)))
-         * .andExpect(status().isOk()) // Asegura que el estatus sea 200 OK
-         * .andExpect(jsonPath("$.message").value("Producto creado con éxito"));
-         * }
-         * 
-         * @Test
-         * void testCreateProductWithInvalidData() throws Exception {
-         * // Usar el constructor para crear un objeto CreateProductDTO con datos
-         * inválidos
-         * CreateProductDTO productDto = new CreateProductDTO(
-         * "", // name vacío
-         * "Esta descripción es demasiado larga para ser aceptada por el DTO. Este texto excede los 100 caracteres permitidos por la anotación @Size."
-         * , // description
-         * // inválida
-         * -5.0, // price negativo
-         * -1L, // stock negativo
-         * -1L, // stockMin negativo
-         * "http://example.com/image.jpg", // imageURL
-         * 1L, // brandId
-         * 2L // subCategoryId
-         * );
-         * 
-         * // Realizar la solicitud POST simulada
-         * mockMvc.perform(post("/product")
-         * .contentType(MediaType.APPLICATION_JSON)
-         * .content(objectMapper.writeValueAsString(productDto)))
-         * .andExpect(status().isBadRequest()) // Asegura que el estatus sea 400 Bad
-         * Request
-         * .andExpect(jsonPath("$.errors.name").value("No puede estar vacio"))
-         * .andExpect(jsonPath("$.errors.price").value("Debe ser mayor o igual a 0"))
-         * .andExpect(jsonPath("$.errors.stock").value("Debe ser mayor o igual a 0"))
-         * .andExpect(jsonPath("$.errors.stockMin").value("Debe ser mayor o igual a 0"))
-         * ;
-         * }
-         */
+
+        @Test
+        @Transactional
+        void testCreateProductWithValidData() throws Exception {
+                CreateProductDTO productDto = new CreateProductDTO(
+                                "Producto de prueba válido",
+                                "descripción",
+                                100.0,
+                                50L,
+                                10L,
+                                "http://example.com/image.jpg",
+                                1L,
+                                1L);
+
+                // Realizar la solicitud POST simulada
+                mockMvc.perform(post("/product")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(productDto)))
+                                .andExpect(status().isCreated()); // Asegura que el estatus sea 200 OK
+                                //.andExpect(jsonPath("$.message").value("Producto creado con éxito"));
+        }
+
+        @Test
+        @Transactional
+        void testCreateProductWithInvalidData() throws Exception {
+                // Usar el constructor para crear un objeto CreateProductDTO con datos inválidos
+                CreateProductDTO productDto = new CreateProductDTO(
+                                "", // name vacío
+                                "Esta descripción es demasiado larga para ser aceptada por el DTO. Este texto excede los 100 caracteres permitidos por la anotación @Size.",
+                                -5.0, // price negativo
+                                -1L, // stock negativo
+                                -1L, // stockMin negativo
+                                "http://example.com/image.jpg", // imageURL
+                                1L, // brandId
+                                2L // subCategoryId
+                );
+
+                // Realizar la solicitud POST simulada
+                mockMvc.perform(post("/product")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(productDto)))
+                                .andExpect(status().isBadRequest()); // Asegura que el estatus sea 400 Bad
+                                //.andExpect(jsonPath("$.errors.name").value("No puede estar vacio"))
+                                //.andExpect(jsonPath("$.errors.price").value("Debe ser mayor o igual a 0"))
+                                //.andExpect(jsonPath("$.errors.stock").value("Debe ser mayor o igual a 0"))
+                                //.andExpect(jsonPath("$.errors.stockMin").value("Debe ser mayor o igual a 0"));
+        }
+
 }
