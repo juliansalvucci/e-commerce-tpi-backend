daac0b53844a85016d556d530584975888bdb212
Felipe Del Zoppo <felipedelzoppo@gmail.com>
2024-10-31 12:00:42 -0300

    feat: crear el servicio findUsers. #45

---
+++ src/main/java/tpi/backend/e_commerce/services/JwtService/FindUserService.java
@@ -0,0 +1,44 @@
+package tpi.backend.e_commerce.services.JwtService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import java.util.Optional;
+
+import tpi.backend.e_commerce.mapper.UserMapper;
+import tpi.backend.e_commerce.models.User;
+import tpi.backend.e_commerce.repositories.IUserRepository;
+import tpi.backend.e_commerce.services.JwtService.interfaces.IFindUserService;
+import tpi.backend.e_commerce.validation.Validation;
+
+public class FindUserService implements IFindUserService{
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Autowired
+    private Validation validation;
+    
+    @Override
+    public ResponseEntity<?> findAllActive() {
+        return ResponseEntity.ok(userRepository.findAllActive());
+    }
+
+    @Override
+    public ResponseEntity<?> findAllDeleted() {
+        return ResponseEntity.ok(userRepository.findAllDeleted());
+    }
+
+    @Override
+    public ResponseEntity<?> findActiveById(Long id) {
+        Optional<User> optionalUser = userRepository.findActiveById(id); 
+        if (optionalUser.isEmpty()){
+            return validation.validate(
+                "id",
+                "No existe un usuario con ese id",
+                404
+            );
+        }
+        return ResponseEntity.ok(UserMapper.toDto(optionalUser.get()));
+    }
+    
+}
