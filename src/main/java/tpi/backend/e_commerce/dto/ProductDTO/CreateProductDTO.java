e98157eade4afac0fa9383d11c7088f905f9ab4a
Felipe Del Zoppo <felipedelzoppo@gmail.com>
2024-10-31 12:01:15 -0300

    chore: Elimine dependencia

--- src/main/java/tpi/backend/e_commerce/dto/ProductDTO/CreateProductDTO.java
+++ src/main/java/tpi/backend/e_commerce/dto/ProductDTO/CreateProductDTO.java
@@ -5,7 +5,6 @@ import org.hibernate.validator.constraints.URL;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import tpi.backend.e_commerce.validation.sizeValidation.SizeValidation;
 
